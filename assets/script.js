3e3221629d09bb91ff2d867787a00db7571f4c3b
Adam <adamgjohnson92@gmail.com>
2023-05-08 13:57:57 -0700

    added query selectors to html elements. Added functions to generate the question with multiple choices, and added event listeners the options buttons. Added conditions for if question is answered correctly or incorrectly. Added feedback for correct or incorrect answers. Added first question to test funcitonality of systems. Added condition for if timer reaches zero, GAME OVER message is displayed and option buttons have display set to none.

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,71 @@
 var beginBtn = document.querySelector("#begin-button");
-var timerEl = document.querySelector("#timer")
-var secondsLeft = 60;
+var timerEl = document.querySelector("#timer");
+var questionEl = document.querySelector("#game-question");
+var choiceBtnArray = document.querySelectorAll(".choice-button")
+var choiceOneBtnEl = document.querySelector("#choice-one");
+var choiceTwoBtnEl = document.querySelector("#choice-two");
+var choiceThreeBtnEl = document.querySelector("#choice-three");
+var choiceFourBtnEl = document.querySelector("#choice-four");
+var feedbackEl = document.querySelector("#feedback")
+var secondsLeft = 3;
 
+var questionOne = {
+  question: "Which coding language is primarily responsible for styling a webpage?",
+  correct: "CSS",
+  incorrectOne: "HTML",
+  incorrectTwo: "JavaScript",
+  incorrectThree: "Python",
+} 
 
+function playGame(){
+  setTime();
+  generateQuestion()
+}
 
 function setTime() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
-      timerEl.textContent = secondsLeft + " seconds left";
+      if (secondsLeft !== 1) {timerEl.textContent = secondsLeft + " seconds left"
+    } else {timerEl.textContent = secondsLeft + " second left"} 
   
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
+        questionEl.textContent = "Game Over"; 
+        questionEl.style.fontSize = "100px"; 
+        choiceOneBtnEl.style.display = "none";
+        choiceTwoBtnEl.style.display = "none";
+        choiceThreeBtnEl.style.display = "none";
+        choiceFourBtnEl.style.display = "none";
       }
     }, 1000);
   }
 
-  beginBtn.addEventListener("click", setTime);
+  function generateQuestion(){
+    questionEl.textContent = questionOne.question;  
+    choiceOneBtnEl.textContent = questionOne.correct;
+    choiceTwoBtnEl.textContent = questionOne.incorrectOne;
+    choiceThreeBtnEl.textContent = questionOne.incorrectTwo;
+    choiceFourBtnEl.textContent = questionOne.incorrectThree;
+    /*for (let i = 1; i < choiceBtnEl.length; i++) {
+        var index = Math.floor(Math.random * choiceBtnEl.length)
+        choiceBtnEl.textContent = questionOne[index]
+      }*/
+    }
+
+  function answerSelection(event){
+      var userAnswer = event.target;
+      console.log(userAnswer)
+      if (userAnswer === questionOne.correct) {
+    score++;
+    feedbackEl.textContent = "Correct!"
+    } else { 
+    feedbackEl.textContent = "Incorrect! -10 Seconds"
+    secondsLeft -10;
+    }
+  }
+
+  for (let i = 0; i < choiceBtnArray.length; i++) {
+      choiceBtnArray[i].addEventListener("click",answerSelection)
+    }
+  beginBtn.addEventListener("click", playGame);
   
